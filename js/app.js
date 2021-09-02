4c2bd1566408dc20a26fd8dc9263baa13b98f10c
shorab-hossain <mdshakiil720516@gmail.com>
2021-09-02 15:55:44 +0600

    update code

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ const searchButton = () =>{
         //show spinner
          toggleSpinner('block'); 
          document.getElementById('book-numbers').style.display = 'none';
-          const url =`http://openlibrary.org/search.json?q=${searchText}`;
+          const url =`https://openlibrary.org/search.json?q=${searchText}`;
            fetch(url)
           .then(res => res.json())
           .then(data => loadBook(data.docs))
